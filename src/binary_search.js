c6beb409f3f167c3d70e595299844b0d6f2926c3
anton morozov <amorozov86@gmail.com>
2022-02-27 17:07:34 +0300

    binary search

---
+++ src/binary_search.js
@@ -0,0 +1,34 @@
+function generateArray(maxNumber) {
+    const array = [];
+    for (let i = 0; i <= maxNumber; i++) {
+        array.push(i);
+    }
+    return array;
+}
+
+function binarySearch(desiredNumber, numbersArray) {
+    searchCallCounter++;
+    const arrayMiddle = Math.floor(numbersArray.length / 2);
+    const middleArrayNumber = numbersArray[arrayMiddle];
+    console.log('middleArrayNumber', middleArrayNumber)
+    if (middleArrayNumber === desiredNumber) {
+        return middleArrayNumber;
+    }
+    if (middleArrayNumber > desiredNumber) {
+        return binarySearch(desiredNumber, numbersArray.slice(0, arrayMiddle))
+    } else {
+        return binarySearch(desiredNumber, numbersArray.slice(arrayMiddle + 1))
+    }
+}
+
+const testArray = generateArray(1024 * 1024 * 10);
+const desiredNumber = Math.floor(Math.random() * testArray.length);
+let searchCallCounter = 0;
+
+console.log('array length = ', testArray.length);
+console.log('desired number: ', desiredNumber);
+
+const resultText = `Number found! It is ${binarySearch(desiredNumber, testArray)}`;
+
+console.log(resultText);
+console.log(`Функция была вызвана ${searchCallCounter} раз/раза`);
